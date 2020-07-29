b30e7937f72d2aad0c60b89b97b9200c6eafafab
Wissanukhong <wissanu.khong@gmail.com>
2020-07-29 16:01:07 +0700

    variables: git commit -m variables

---
+++ 01_variables/variable.js
@@ -0,0 +1,16 @@
+// var
+var fruits = "mago";
+
+console.log(fruits);
+
+// let
+let yourName = "John Perci";
+
+console.log(yourName);
+
+// const
+const website = "www.google.com";
+
+console.log(website);
+
+
