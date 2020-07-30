a349536dc5436f9cd8c14b470e49f432d6966025
Wissanukhong <wissanu.khong@gmail.com>
2020-07-30 22:00:16 +0700

    data type part II

--- 02_DataTypes/dataType.js
+++ 02_DataTypes/dataType.js
@@ -3,3 +3,19 @@
 // Number
 let number = 1;
 console.log(number);
+
+// String 
+let yourName = "John";
+console.log(yourName);
+
+// undefined
+let a;
+console.log(a)
+
+// Null
+let b = null;
+console.log(b);
+
+// Symbol
+let id = Symbol("id");
+alert(id.toString()); // Symbol(id), now it works
