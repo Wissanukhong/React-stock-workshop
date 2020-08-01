daf0860e2883c4fd622221b9bf6dbfd837be8891
Wissanukhong <wissanu.khong@gmail.com>
2020-08-01 22:34:22 +0700

    add event search

--- 07_DOMmanipulations/DOMmanipulations.js
+++ 07_DOMmanipulations/DOMmanipulations.js
@@ -1,8 +1,12 @@
-function appendImageElem () {
-    
+function appendImageELem(keyword, index) {
+    const imgElem = document.createElement('img');
+    imgElem.src= `https://source.unspash.com/400x225/?${keyword}&sig${index}`;
+
+    const galleryElem = document.querySelector('.gallery');
+    galleryElem.appendChild(imgElem);
 }
 
-function run () {
+function run() {
 
 }
 
