48a4aaa13887751a0c7ffe9630cbd78bd90c5ea6
Wissanukhong <wissanu.khong@gmail.com>
2020-08-01 22:57:28 +0700

    Arithetic Operator

---
+++ 03_AritheticOperator/AritheticOperator.js
@@ -0,0 +1,14 @@
+// Arithetic Operator คือ ตัวดำเนินการทางคณิณศาสตร์ โดยจะทำหน้าที่ บวก ลบ คูณ หาร ยกกำลัง หรือว่า หารลงตัวนั้นเอง 
+// Example 
+// ทุกบิลล์ เมื่อซื้อสินค้าเสร็จจะต้องถูก บวกภาษีเพิ่มอีก 7 %
+let bill = 523;
+let Vat = (bill * 7) / 100;
+
+let total = bill + Vat;
+console.log(total);
+
+let yearBirth = 1993;
+let currentYear = 2020;
+
+let yearOdls = currentYear - yearBirth;
+console.log(yearOdls);
