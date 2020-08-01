97c45a5ac496f0721de57df7e6fbf7fd34256e1e
Wissanukhong <wissanu.khong@gmail.com>
2020-08-01 22:44:39 +0700

    debug DOMmanipulation

--- 07_DOMmanipulations/DOMmanipulations.js
+++ 07_DOMmanipulations/DOMmanipulations.js
@@ -1,13 +1,15 @@
 function appendImageELem(keyword, index) {
     const imgElem = document.createElement('img');
-    imgElem.src= `https://source.unspash.com/400x225/?${keyword}&sig${index}`;
+    imgElem.src= `https://source.unsplash.com/400x225/?${keyword}&sig=${index}`;
 
     const galleryElem = document.querySelector('.gallery');
     galleryElem.appendChild(imgElem);
 }
 
 function run() {
-
+    for (let i = 1; i <= 9; i++) {
+        appendImageELem('cat', i);
+    }
 }
 
 run();
