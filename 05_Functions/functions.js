c42b5142d1442172124684473fd72e3eb1053c82
Wissanukhong <wissanu.khong@gmail.com>
2020-08-02 23:24:23 +0700

    write a document

--- 05_Functions/functions.js
+++ 05_Functions/functions.js
@@ -12,3 +12,15 @@ function calculateVat(money, vat) {
 
 const totalVat = calculateVat(10000, 7);
 console.log(totalVat);
+
+// function yearOld
+let JohnBith = 2000;
+let currentYear = 2020;
+
+function yearOld() {
+   let year = currentYear - JohnBirth;
+   console.log(year);
+}
+
+yearOld()
+
--- 06_Flowcontrols(loop)/loop.js
+++ 06_Flowcontrols(loop)/loop.js
@@ -6,3 +6,22 @@ for (let i = 0; i < 10; i++) {
 for (let counter = 0 ; counter < 10; counter++) {
     console.log(counter);
 }
+
+// while loop example 1
+var b = [123,12,1];
+var i = 0;
+while(i < b.length) {
+    var c = b[i] * 2
+    console.log(c)
+    i++
+}
+
+// while loop example 2
+var allBooks = ['Jonash', 'Auro x', 'Long learn'];
+var i = 0;
+
+while(i < allBooks.length) {
+    var book = allBooks[i];
+    console.log(book);
+    i++
+}
